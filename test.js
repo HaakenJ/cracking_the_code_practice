aac034f6bbaec016a532166294fa7852d36f013d
Haakasaurus <kramerhjohnson@gmail.com>
2020-06-16 11:18:59 -0700

    Added package.json, mocha, chai, and test suite

---
+++ test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+// Arrays and Strings
+const rotateMatrixInPlace = require('./arrays_and_strings/rotateMatrixInPlace');
+const rotateMatrix = require('./arrays_and_strings/rotateMatrix');
+
+describe('Rotate Matrix', () => {
+    const matrix3 = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+    
+    const matrix4 = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+    ]
+    
+    const matrix5= [
+        [1,2,4,5],
+        [6,7,8,9,10],
+        [11,12,13,14,15],
+        [16,17,18,19,20],
+        [21,22,23,24,25]
+    ]
+
+    instanceof()
+})
