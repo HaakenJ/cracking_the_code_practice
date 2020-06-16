84d402d9688151b11cee09195071e18686523b08
Haakasaurus <kramerhjohnson@gmail.com>
2020-06-16 11:10:01 -0700

    Solved rotating matrix in place

--- arrays_and_strings/rotateMatrixInPlace.js
+++ arrays_and_strings/rotateMatrixInPlace.js
@@ -1,5 +1,18 @@
-const matrix = [
-    [1,2,3,4,5],
+const matrix3 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+const matrix4 = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+
+const matrix5= [
+    [1,2,4,5],
     [6,7,8,9,10],
     [11,12,13,14,15],
     [16,17,18,19,20],
@@ -7,5 +20,21 @@ const matrix = [
 ]
 
 function rotateMatrix(matrix) {
-    
+    if (matrix.length <= 1) return matrix;
+    const n = matrix.length - 1;
+
+    for (let first=0, last=n; first<last; first++, last--) {
+        for (let offset = 0; first+offset < last; offset++) {
+            const temp = matrix[first][first + offset];
+            matrix[first][first + offset] = matrix[last - offset][first];
+            matrix[last - offset][first]  = matrix[last][last - offset];
+            matrix[last][last - offset]   = matrix[first + offset][last];
+            matrix[first + offset][last]  = temp;
+        }
+    }
+    return matrix;
 }
+
+console.log(rotateMatrix(matrix3));
+console.log(rotateMatrix(matrix4));
+console.log(rotateMatrix(matrix5));
