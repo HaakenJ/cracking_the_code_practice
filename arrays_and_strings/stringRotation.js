dcabe0fb437a3d6110edf46dcf24a3fd3a5ba701
Haakasaurus <kramerhjohnson@gmail.com>
2020-06-22 11:52:32 -0700

    Added book solution to string rotation

--- arrays_and_strings/stringRotation.js
+++ arrays_and_strings/stringRotation.js
@@ -1,11 +1,20 @@
-function stringRotation(s1, s2) {
-    let start = 0;
-    while (s2.indexOf(s1[0], start) >= 0) {
-        let firstLetter = s2.indexOf(s1[0], start);
-        let swapped = s2.slice(firstLetter,) + s2.slice(0, firstLetter);
+// function stringRotation(s1, s2) {
+//     let start = 0;
+//     while (s2.indexOf(s1[0], start) >= 0) {
+//         let firstLetter = s2.indexOf(s1[0], start);
+//         let swapped = s2.slice(firstLetter,) + s2.slice(0, firstLetter);
 
-        if (s1.includes(swapped)) return true;
-        start++;
+//         if (s1.includes(swapped)) return true;
+//         start++;
+//     }
+//     return false;
+// }
+
+function stringRotation(s1, s2) {
+    const len = s1.length;
+    if (len === s2.length && len > 0) {
+        const s1s1 = s1 + s1;
+        return s1s1.includes(s2);
     }
     return false;
 }
