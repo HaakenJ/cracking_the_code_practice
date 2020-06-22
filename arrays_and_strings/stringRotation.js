59f3c2e3beddbca993334946e3fd2e3c79edad58
Haakasaurus <kramerhjohnson@gmail.com>
2020-06-22 11:46:00 -0700

    Completed first draft of stringRotation

--- arrays_and_strings/stringRotation.js
+++ arrays_and_strings/stringRotation.js
@@ -1,6 +1,13 @@
 function stringRotation(s1, s2) {
     let start = 0;
     while (s2.indexOf(s1[0], start) >= 0) {
-        
+        let firstLetter = s2.indexOf(s1[0], start);
+        let swapped = s2.slice(firstLetter,) + s2.slice(0, firstLetter);
+
+        if (s1.includes(swapped)) return true;
+        start++;
     }
+    return false;
 }
+
+module.exports = stringRotation;
