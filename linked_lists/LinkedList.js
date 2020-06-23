c0a98c8415ae29585d2547cacbbf0c80b8be7c99
Haakasaurus <kramerhjohnson@gmail.com>
2020-06-23 12:09:42 -0700

    Added toArray and printList methods to LinkedList along with tests

--- linked_lists/LinkedList.js
+++ linked_lists/LinkedList.js
@@ -2,6 +2,26 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    printList = () => {
+        let n = this.head;
+        while (n.next !== null) {
+            console.log(n.data);
+            n = n.next;
+        }
+        console.log(n.data);
+    }
+
+    toArray = () => {
+        let n = this.head;
+        let result = [];
+        while (n.next !== null) {
+            result.push(n.data);
+            n = n.next;
+        }
+        result.push(n.data);
+        return result;
+    }
 }
 
 class Node {
@@ -15,3 +35,20 @@ module.exports = {
     LinkedList: LinkedList,
     Node: Node
 }
+
+// const ll = new LinkedList;
+// const first = new Node(1);
+// const second = new Node(2);
+// const third = new Node(2);
+// const fourth = new Node(3);
+// const fifth = new Node (4);
+// const sixth = new Node(4);
+
+// ll.head = first;
+// first.next = second;
+// second.next = third;
+// third.next = fourth;
+// fourth.next = fifth;
+// fifth.next = sixth;
+
+// ll.printList();
--- linked_lists/test.js
+++ linked_lists/test.js
@@ -1,3 +1,32 @@
 const { LinkedList, Node } = require('./LinkedList');
 const chai = require('chai');
 const expect = chai.expect;
+
+// const deleteDupes = require('./deleteDupes');
+
+const ll = new LinkedList;
+const first = new Node(1);
+const second = new Node(2);
+const third = new Node(2);
+const fourth = new Node(3);
+const fifth = new Node (4);
+const sixth = new Node(4);
+
+ll.head = first;
+first.next = second;
+second.next = third;
+third.next = fourth;
+fourth.next = fifth;
+fifth.next = sixth;
+
+describe('Linked List Methods', () => {
+    it('Should return an array of the linked list contents', () => {
+        expect(ll.toArray()).to.deep.equal([1,2,2,3,4,4]);
+    })
+})
+
+// describe('Delete Duplicates', () => {
+//     it('Should remove duplicates from the linked list', () => {
+//         expect(deleteDupes(ll.head)).to.
+//     })
+// })
