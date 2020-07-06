117311b6baf5dd49a60f754990e2f05a7926d52a
Haakasaurus <kramerhjohnson@gmail.com>
2020-07-06 13:30:20 -0700

    Added first solution for finding the interection between two lists

---
+++ linked_lists/intersection.js
@@ -0,0 +1,17 @@
+const { LinkedList, Node } = require('./LinkedList');
+
+const findIntersection = (head1, head2) => {
+    const map = new Map();
+
+    while(head1 !== null) {
+        map.set(head1, true);
+        head1 = head1.next;
+    }
+
+    while(head2 !== null) {
+        if (map.has(head2)) return head2;
+        head2 = head2.next;
+    }
+
+    return false;
+}
