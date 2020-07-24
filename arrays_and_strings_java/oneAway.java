05809a06a971f77034affd924e89f7c79f9da3cc
Haakasaurus <kramerhjohnson@gmail.com>
2020-07-23 18:02:25 -0700

    Added tests for oneAway.java

--- arrays_and_strings_java/oneAway.java
+++ arrays_and_strings_java/oneAway.java
@@ -1,5 +1,5 @@
 public class oneAway {
-    boolean needsOnereplacement(String s1, String s2) {
+    public static boolean needsOnereplacement(String s1, String s2) {
         boolean foundDiff = false;
         for (int i =0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
@@ -10,7 +10,7 @@ public class oneAway {
         return true;
     }
 
-    boolean needsOneInsert(String s1, String s2) {
+    public static boolean needsOneInsert(String s1, String s2) {
         int i = 0;
         int j = 0;
         while (j < s2.length() && i < s1.length()) {
@@ -25,8 +25,8 @@ public class oneAway {
         return true;
     }
 
-    boolean isOneEditAway(String s1, String s2) {
-        if (s1.length == s2.length) {
+    public static boolean isOneEditAway(String s1, String s2) {
+        if (s1.length() == s2.length()) {
             return needsOnereplacement(s1, s2);
         } else if (s1.length() + 1 == s2.length()) {
             return needsOneInsert(s1, s2);
@@ -34,5 +34,32 @@ public class oneAway {
             return needsOneInsert(s2, s1);
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hellf";
+        assert(isOneEditAway(s1, s2));
+        s1 = "hello";
+        s2 = "hell";
+        assert(isOneEditAway(s1, s2));
+        s1 = "hello";
+        s2 = "hello";
+        assert(isOneEditAway(s1, s2));
+        s1 = "hemlo";
+        s2 = "hello";
+        assert(isOneEditAway(s1, s2));
+        s1 = "aello";
+        s2 = "hello";
+        assert(isOneEditAway(s1, s2));
+
+        s1 = "aallo";
+        s2 = "hello";
+        assert(!isOneEditAway(s1, s2));
+        s1 = "llo";
+        s2 = "hello";
+        assert(!isOneEditAway(s1, s2));
+
+        System.out.println("All tests passed");
     }
 }
