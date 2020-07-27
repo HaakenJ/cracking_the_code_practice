5a2119e93cf3f368531864bb52ad4f57e99a3555
Haakasaurus <kramerhjohnson@gmail.com>
2020-07-27 16:15:10 -0700

    Started checkPalindrome.java, got replace and sort to work

--- arrays_and_strings_java/CheckPalindrome.java
+++ arrays_and_strings_java/CheckPalindrome.java
@@ -1,8 +1,26 @@
-class CheckPalindrome {
-    public static boolean isPalindrome(s)
+import java.util.Arrays;
+
+class checkPalindrome {
+    public static boolean isPalindrome(String s) 
     {
         if (s.length() > 1) {
-            
+            String sortedStr = s.toLowerCase();
+            sortedStr = sortedStr.replaceAll("\\s+", "");
+            Character[] charArray = new Character[sortedStr.length()];
+            for (int i=0; i < sortedStr.length(); i++) {
+                charArray[i] = sortedStr.charAt(i);
+            }
+            Arrays.sort(charArray);
+
+            for (char c: charArray) {
+                System.out.println(c);
+            }
         }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        isPalindrome("he l lo");
     }
 }
