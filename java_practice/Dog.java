7916397019b37e27efa58c9891ef9b88dbdcab39
Haakasaurus <kramerhjohnson@gmail.com>
2020-08-08 08:42:18 -0700

    Exercise 7

--- java_practice/Dog.java
+++ java_practice/Dog.java
@@ -1,24 +1,12 @@
 class Dog {
-    Dog(int i) {
+
+    public void hi() {
         System.out.println("Woof");
     }
 
-    Dog(float f) {
-        System.out.println("Howwwwwwllll");
-    }
-
-    Dog(char c, double d) {
-        System.out.println("Sniff Sniff");
-    }
-
-    Dog(double d, char c) {
-        System.out.println("Yelp");
-    }
-
     public static void main(String[] args) {
-        Dog fido = new Dog(1);
-        Dog rover = new Dog(2.0f);
-        Dog rex = new Dog('r', 2.43d);
-        Dog harry = new Dog(25.25d, 'r');
+        Dog rex = new Dog();
+
+        rex.hi();
     }
 }
