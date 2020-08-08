7d1f3ea2420cda8c2aa354e499c7317b159ea6b6
Haakasaurus <kramerhjohnson@gmail.com>
2020-08-08 09:01:51 -0700

    Exercise 9 I think

--- java_practice/Dog.java
+++ java_practice/Dog.java
@@ -1,12 +1,26 @@
 class Dog {
+    String s = "initial";
+    int i = 0;
 
-    public void hi() {
-        System.out.println("Woof");
+    Dog(String s) {
+        this.s = s;
+        System.out.println("String const");
+    }
+
+    Dog(int i) {
+        this.i = i;
+        System.out.println("int const");
+    }
+
+    Dog() {
+        this("New string");
+        i = 5;
+        System.out.println("s is now: " + s);
+        System.out.println("i is now: " + i);
     }
 
     public static void main(String[] args) {
         Dog rex = new Dog();
-
-        rex.hi();
+        
     }
 }
