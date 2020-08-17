32d7df84c993aec9482a6cdbfd8142e483f29d58
Haakasaurus <kramerhjohnson@gmail.com>
2020-08-17 14:46:37 -0700

    Solved exercise 14

--- java_practice/Dog.java
+++ java_practice/Dog.java
@@ -1,37 +1,17 @@
 class Dog {
-    String s = "initial";
-    int i = 0;
+    static String s = "Woof wooof";
+    static int i;
 
-    Dog(String s) {
-        this.s = s;
-        System.out.println("String const");
+    static {
+        i = 2;
     }
 
-    Dog(int i) {
-        this.i = i;
-        System.out.println("int const");
-    }
-
-    void bark() {
-        System.out.println("Bark Bark");
-    }
-
-    Dog() {
-        this("New string");
-        i = 5;
-        System.out.println("s is now: " + s);
-        System.out.println("i is now: " + i);
-    }
-
-    protected void finalize() {
-        System.out.println("Dog has been finalized");
+    static void bark() {
+        System.out.println(s);
+        System.out.println(i);
     }
 
     public static void main(String[] args) {
-        Dog rex = new Dog();
-
-        rex.bark();
-        System.out.println("Post bark");
-        System.gc();
+        bark();
     }
 }
